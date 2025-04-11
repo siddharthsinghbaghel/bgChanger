9e7c46db9edc6635bfd3d220f72aefeac3d7fbd2
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2025-04-11 21:17:06 +0530

    App.js

--- bgchangerhit/src/App.jsx
+++ bgchangerhit/src/App.jsx
@@ -7,7 +7,12 @@ function App() {
     <div className="w-full h-screen duration-200 
     "
     style={{backgroundColor:color}}>
-      <div className='text-green-500'>hellojii</div>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+          <button onClick={() => setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg *
+          ' style={{backgroundColor: "green"}}>green</button>
+        </div>
+      </div>
     </div>
   )
 }
