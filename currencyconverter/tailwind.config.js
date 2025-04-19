6a2a14d1b4fb53b258edf1120388cf846cb6f97d
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2025-04-19 09:22:49 +0530

    index.html

---
+++ currencyconverter/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ✅ This tells Tailwind to look into all JS/JSX/TS/TSX files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
