edd10ddd26acd432da6a84a6ff34c969d89b7854
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2025-04-19 11:17:20 +0530

    App.js

--- currencyconverter/src/components/InputBox.jsx
+++ currencyconverter/src/components/InputBox.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { useId } from 'react'
 function InputBox({
     label,
     amount,
     onAmountChange,
     onCurrencyChange,
     currencyOptions = [],
-    selectCurrency = "usd",
+    selectCurrency = "USD",
     amountDisable = false,
     currencyDisable = false,
 
@@ -25,23 +25,26 @@ function InputBox({
                 className='outline-none w-full bg-transparent py-1.5'
                 placeholder="Amount"
                 disabled={amountDisable}
-                value={amount}
+                value={amount || ""}
                 onChange= {(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className='w-1/2 flex flex-wrap justify-end text-right'>
             <p className='text-black/40 mb-2 w-full'>Currency type</p>
             <select 
-            className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-            value={selectCurrency}
-            onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-            disabled={currencyDisable}
-            >
-                {currencyOptions.map((currency) =>
-                    (
-                    <option key ={currency} value = {currency}>
-                    </option>))}
-            </select>
+  className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+  value={selectCurrency}
+  onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+  disabled={currencyDisable}
+>
+  <option value="" disabled>Select</option>
+  {currencyOptions.map((currency) => (
+    <option key={currency} value={currency}>
+      {currency.toUpperCase()}
+    </option>
+  ))}
+</select>
+
             </div>
         </div>
     );
--- currencyconverter/src/hooks/useCurrencyInfo.js
+++ currencyconverter/src/hooks/useCurrencyInfo.js
@@ -1,15 +1,21 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data , setData ] = useState({})
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);
-    },[currency])
-    // console.log(data);
-    return data
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    if (!currency) return;
+
+    fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.rates))
+      .catch((err) => {
+        console.error("Failed to fetch currency data:", err);
+        setData({});
+      });
+  }, [currency]);
+
+  return data;
 }
 
 export default useCurrencyInfo;
